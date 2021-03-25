6c2e3556067b3544b91cba9f5cf7ec5e158942ff
ImNM <water0641@naver.com>
2021-03-25 18:37:18 +0900

    testcase 11

--- index.js
+++ index.js
@@ -116,12 +116,11 @@ app.get('/api/hello',(req,res)=>{
     res.send("테스트ㅎㅇㅎㅇgdgdㅎㅇㅎㅇ");
 ;})
 
-if(process.env.NODE_ENV ==='production'){
-    app.use(express.static(path.join(__dirname,'../client/build')));
-}
+app.use(express.static(path.join(__dirname,'../client/build')));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname+'../client/build/index.html'));
 })
 
 
