f58fdd0fc783ba5371d559b0c7aee6db79c5756e
ImNM <water0641@naver.com>
2021-03-29 22:56:30 +0900

    test

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -2,8 +2,8 @@ import React,{useState} from 'react'
 import {useDispatch} from 'react-redux';
 import {registerUser} from '../../../_actions/user_action.js';
 import {withRouter} from'react-router-dom';
-
-
+import KakaoLogin from 'react-kakao-login';
+import axios from 'axios';
 function RegisterPage(props) {
     const dispatch = useDispatch();
 
@@ -47,11 +47,25 @@ function RegisterPage(props) {
             }
         })
     }
+    const javatoken = '39fc87a51dc30c1623a74a70104112da'; 
+    const onkakaoLoginSuccesshandler = (data)=>{
+        console.log(data.response.access_token);
+        axios.post('/api/users/oauth/kakao/login',{accessToken : data.response.access_token})
+        .then(response => {//오류코드 추가해야햄
+            console.log();
+            localStorage.setItem(
+                "Auth",
+                JSON.stringify({ token : response.data.token , name : response.data.name })
+              );
+              props.history.push('/');
+        });
+    }
+  
 
     return (
         <div style={{
             display:'flex', justifyContent:'center',alignItems:'center'
-            ,width:'100%',height:'100vh'
+            ,width:'100%',height:'100vh',flexDirection:'column'
         }}>
             <form style = {{display:'flex',flexDirection:'column'}}
                 onSubmit = {onSubmitHandler}
@@ -68,7 +82,20 @@ function RegisterPage(props) {
                 <button>
                     Register
                 </button>
+                <br/>
+               
             </form>
+
+            <KakaoLogin
+            token={javatoken}
+           onSuccess={onkakaoLoginSuccesshandler}
+             onFail={console.error}
+           onLogout={console.info}
+           useLoginForm>
+                 <div >카카오톡으로 회원가입하기</div>
+           </KakaoLogin>
+          
+             
         </div>
     )
 }
